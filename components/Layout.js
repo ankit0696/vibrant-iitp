88c258b723ef70772415d7609d0e695b0bd0885b
Ankit Kumar <ankitkr0696@gmail.com>
2022-09-15 01:31:07 +0530

    toast import fix

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { Toaster } from 'react-hot-toast'
 
 export default function Layout({
   title = '',
