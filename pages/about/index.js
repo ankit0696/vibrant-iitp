b7f524de6cc2f75cee12c71fc757c85e2496ccd9
Ankit Kumar <ankitkr0696@gmail.com>
2022-09-17 19:22:04 +0530

    about page team added

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,12 @@
 import AboutHero from '@/components/about/AboutHero'
+import Team from '@/components/about/Team'
 import Layout from '@/components/Layout'
 
 export default function AboutPage() {
   return (
     <Layout>
       <AboutHero />
+      <Team />
     </Layout>
   )
 }
---
+++ pages/account/student/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function StudentDashboardPage() {
+  return <div>StudentDashboardPage</div>
+}
