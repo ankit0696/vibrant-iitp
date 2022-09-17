ef1523b07910feb11a3826f5a225c17fb2db6ad6
Ankit Kumar <ankitkr0696@gmail.com>
2022-09-17 14:13:59 +0530

    header added

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout'
 export default function Home() {
   return (
     <Layout title='' keywords='' description=''>
-      <main></main>
+      <main>Body</main>
     </Layout>
   )
 }
